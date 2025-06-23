2251536c59ee631ec18a2f12899454fb324c1328
jopeodelas <2081321@student.uma.pt>
2025-06-23 22:56:54 +0100

    Fix double Router error by removing BrowserRouter from main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,14 +4,11 @@ console.log('🔥 EMERGENCY: API URL overridden to:', (window as any).VITE_API_U
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
