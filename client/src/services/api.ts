1f19eff6642cd67cf8496de6ca161cc7c3521e38
jopeodelas <2081321@student.uma.pt>
2025-06-23 17:47:11 +0100

    update

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiBaseUrl = import.meta.env.VITE_API_URL || 'http://13.60.228.50/api';
+const apiBaseUrl = import.meta.env.VITE_API_URL || 'https://api.maritimofans.pt/api';
 
 const api = axios.create({
   baseURL: apiBaseUrl,
